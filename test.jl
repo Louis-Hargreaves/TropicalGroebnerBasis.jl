b85caa2461d503f98b85771b43322a4075856c26
Louis-Hargreaves <louishargreaves0@gmail.com>
2025-02-18 16:44:56 +0000

    changes

--- test.jl
+++ test.jl
@@ -1,5 +1,29 @@
-#A function that takes a string and returns the number of words in the string.
+using Oscar
 
-function count_words(str::String)
-    return length(split(str))
-end
+#Write something which takes a list of polynomials and a performs Oscar.tighten_simulation on them
+
+#Define the prime p for the p-adic valuation
+p = 2
+nu_p = tropical_semiring_map(QQ, p)
+
+#Start with the polynomial ring
+Rtx, (t, x1, x2, x3) = polynomial_ring(ZZ, [:t, :x1, :x2, :x3], internal_ordering=:deglex)
+
+#Define the polynomials
+g1 = x1^2 + t*x2^2 - t^2*x3^2
+g2 = t*x1^2 + x2^2 + (t + t^2)*x3^2
+g3 = t^4*x1^2 + (t^4 + t^5)*x2^2 + t^3*x3^2 
+G = [g1, g2, g3]
+
+#Write out the function which performs Oscar.tighten_simulation
+
+G_red = tighten_simulation.(G, Ref(nu_p))
+
+#Define the ordering on homogeneous functions
+M =[-1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]
+o1 = matrix_ordering(Rtx, M)
+leading_monomial.(G_red, ordering=o1)
+
+#Sort by leading monomial
+G_red = sort(G_red, by = x -> leading_monomial(x, ordering=o1), rev=true)
+leading_monomial(G_red[1], ordering=o1)
